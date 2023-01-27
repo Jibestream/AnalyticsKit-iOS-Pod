1c10f2caa28eed5be48c49846a096f2484738236
developer <developer@jibestream.com>
2023-01-27 23:28:01 +0000

    Travis-CI POD Update: 38

---
+++ AnalyticsKit-iOS-Pod/Frameworks/JMapAnalyticsKit.xcframework/ios-arm64_i386_x86_64-simulator/JMapAnalyticsKit.framework/Headers/JMapAnalyticsKit.h
@@ -0,0 +1,16 @@
+//
+//  JMapAnalyticsKit.h
+//  JMapAnalyticsKit
+//
+//  Created by Louie Yuen on 2017-12-05.
+//  Copyright © 2017 Jibestream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+#import <JMapAnalyticsKit/JMapAnalytics.h>
+#import <JMapAnalyticsKit/JMapAnalytics.h>
+
+
+
